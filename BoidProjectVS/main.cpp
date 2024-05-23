a1bbd7aa9dbfc678ff5fe8f130ea72adb5afe52b
Ellmaer <ellmaer2003@gmail.com>
2024-05-22 20:31:15 -0700

    raylib.h works!
    -the following in the main.cpp file is just an example window that comes up to see if the raylib.h works

--- BoidProjectVS/main.cpp
+++ BoidProjectVS/main.cpp
@@ -1,22 +1,47 @@
-// BoidProjectVS.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Main file
 
 #include <iostream>
-#include "secondary.h"
+#include <raylib.h>
 
-int main()
-{
-    NoClass obj;
-    obj.sayNo();
+using namespace std;
+
+int main() {
+
+    Color Dark_Green = Color{ 20, 160, 133, 255 };
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    int ball_x = 100;
+    int ball_y = 100;
+    int ball_speed_x = 5;
+    int ball_speed_y = 5;
+    int ball_radius = 15;
+
+    cout << "Hello World" << endl;
+
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        ClearBackground(Dark_Green);
+        ball_x += ball_speed_x;
+        ball_y += ball_speed_y;
+
+        if (ball_x + ball_radius >= screenWidth || ball_x - ball_radius <= 0)
+        {
+            ball_speed_x *= -1;
+        }
+
+        if (ball_y + ball_radius >= screenHeight || ball_y - ball_radius <= 0)
+        {
+            ball_speed_y *= -1;
+        }
+
+        DrawCircle(ball_x, ball_y, ball_radius, WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
