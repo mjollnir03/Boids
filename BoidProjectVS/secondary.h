e4af8a221c55007435dfedf449bc427eec164558
Ellmaer <ellmaer2003@gmail.com>
2024-05-22 20:08:20 -0700

    Add project files.

---
+++ BoidProjectVS/secondary.h
@@ -0,0 +1,13 @@
+#ifndef NO_CLASS_H
+#define NO_CLASS_H
+
+#include <iostream>
+
+class NoClass {
+public:
+    void sayNo() {
+        std::cout << "noooo" << std::endl;
+    }
+};
+
+#endif // NO_CLASS_H
