bb09c4d482ef36802aac6d198c2f882afa082fe2
Ellmaer <ellmaer2003@gmail.com>
2024-05-24 13:30:06 -0700

    just some extra comments
    -this files is just to store any extra functions I used in the boid.h file

--- BoidProjectVS/functions.h
+++ BoidProjectVS/functions.h
@@ -1,13 +1,17 @@
-#pragma once
+#pragma once // Include guard to prevent multiple inclusion of this header file
+
+// Template function to clamp a value between a minimum and maximum value
 template<typename T>
 T Clamp(T value, T min, T max) {
+    // Check if the value is less than the minimum
     if (value < min) {
-        return min;
+        return min; // Return the minimum value
     }
+    // Check if the value is greater than the maximum
     else if (value > max) {
-        return max;
+        return max; // Return the maximum value
     }
     else {
-        return value;
+        return value; // Return the original value if within the range
     }
 }
