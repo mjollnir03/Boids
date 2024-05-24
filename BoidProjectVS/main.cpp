f4bfbbe1bb4039b970f31f4aed842d0d67822a38
Ellmaer <ellmaer2003@gmail.com>
2024-05-23 21:37:41 -0700

    playing around with boid.h class

--- BoidProjectVS/main.cpp
+++ BoidProjectVS/main.cpp
@@ -13,19 +13,29 @@ int main() {
     int centerX = screenWidth / 2; 
     int centerY = screenHeight / 2;  
     
-    Boid* boidptr = new Boid(centerX, centerY, { 0.0f, 0.0f }); 
+    Boid* boidptr = new Boid(centerX, centerY, 6.0f, 2.0f); 
+    Boid* boidptr2 = new Boid(centerX, centerY, -6.0f, -2.0f);
 
     InitWindow(screenWidth, screenHeight, "Boids - By Ellmaer Ranjber");
     SetTargetFPS(60);
-    HideCursor();
+    //HideCursor();
 
     while (WindowShouldClose() == false) {
 
         BeginDrawing();
+        
+        boidptr->Update();
+        boidptr2->Update();
 
         ClearBackground(backgroundColor);
         
         boidptr->DrawBoid();
+        boidptr2->DrawBoid();
+
+        // Check for perimeter collision
+        if (boidptr->IsWithinPerimeter(*boidptr2)) {
+            DrawText("Collision Detected!", screenWidth / 2 - MeasureText("Collision Detected!", 20) / 2, screenHeight / 2, 20, RED);
+        }
          
 
         EndDrawing();
