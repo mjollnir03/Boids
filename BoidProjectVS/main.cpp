6eae42445546cfea6450097bec9e30692225fd57
Ellmaer <ellmaer2003@gmail.com>
2024-06-05 17:59:08 -0700

    update main.cpp
    - I have added an intro screen that shows the user the raylib icon

--- BoidProjectVS/main.cpp
+++ BoidProjectVS/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream> // Include standard input/output stream library
 #include <raylib.h> // Include Raylib library for graphics
-#include <raymath.h>
+#include <raymath.h> // Include Raylib math library
 #include <cstdlib> // Include standard library for various utilities
-#include <random>
+#include <random> // Include the random library
 #include "boid.h" // Include user-defined Boid class header file
 
 #define NUM_BOIDS 500 // Define the number of boids in the simulation
@@ -10,14 +10,15 @@
 // Function prototypes
 void InitializeBoids(Boid* boids[], int screenWidth, int screenHeight);
 void UpdateBoids(Boid* boids[], int screenWidth, int screenHeight);
+void PlayRaylibLogoAnimation(int screenWidth, int screenHeight); // Taken from the raylib examples 
 
 int main() {
     // Define colors for background and boids
     Color backgroundColor = BLACK;
 
     // Define screen dimensions
-    const int screenWidth = 1000;
-    const int screenHeight = 1000;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 
     // Seed the random number generator with current time
     srand(static_cast<unsigned>(time(0)));
@@ -31,7 +32,10 @@ int main() {
     // Initialize the window with specified dimensions and title
     InitWindow(screenWidth, screenHeight, "Boids - By Ellmaer Ranjber");
     SetTargetFPS(60); // Set target frames per second for rendering
-    //HideCursor(); // Hide cursor during simulation (optional)
+    // HideCursor(); // Hide cursor during simulation (optional)
+
+    // Play the Raylib screen intro
+    PlayRaylibLogoAnimation(screenWidth, screenHeight);
 
     // Main simulation loop
     while (WindowShouldClose() == false) {
@@ -136,4 +140,133 @@ void UpdateBoids(Boid* boids[], int screenWidth, int screenHeight) {
         // Draw the current boid on the screen
         boids[i]->DrawBoid();
     }
+}
+
+void PlayRaylibLogoAnimation(int screenWidth, int screenHeight)
+{
+    // Local Variables
+    int logoPositionX = screenWidth / 2 - 128;
+    int logoPositionY = screenHeight / 2 - 128;
+
+    int framesCounter = 0;
+    int lettersCount = 0;
+
+    int topSideRecWidth = 16;
+    int leftSideRecHeight = 16;
+
+    int bottomSideRecWidth = 16;
+    int rightSideRecHeight = 16;
+
+    int state = 0; // Tracking animation states (State Machine)
+    float alpha = 1.0f; // Useful for fading
+    float fadeOutAlpha = 0.0f; // Fade out alpha value
+
+    
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Update
+        if (state == 0) // State 0: Small box blinking
+        {
+            framesCounter++;
+
+            if (framesCounter == 120)
+            {
+                state = 1;
+                framesCounter = 0; // Reset counter... will be used later...
+            }
+        }
+        else if (state == 1) // State 1: Top and left bars growing
+        {
+            topSideRecWidth += 4;
+            leftSideRecHeight += 4;
+
+            if (topSideRecWidth == 256) state = 2;
+        }
+        else if (state == 2) // State 2: Bottom and right bars growing
+        {
+            bottomSideRecWidth += 4;
+            rightSideRecHeight += 4;
+
+            if (bottomSideRecWidth == 256) state = 3;
+        }
+        else if (state == 3) // State 3: Letters appearing (one by one)
+        {
+            framesCounter++;
+
+            if (framesCounter / 12) // Every 12 frames, one more letter!
+            {
+                lettersCount++;
+                framesCounter = 0;
+            }
+
+            if (lettersCount >= 10) // When all letters have appeared, just fade out everything
+            {
+                alpha -= 0.02f;
+
+                if (alpha <= 0.0f)
+                {
+                    alpha = 0.0f;
+                    state = 4;
+                }
+            }
+        }
+        else if (state == 4) // State 4: Fade to black
+        {
+            fadeOutAlpha += 0.01f;
+
+            if (fadeOutAlpha >= 1.0f)
+            {
+                fadeOutAlpha = 1.0f;
+            }
+        }
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        if (state == 0)
+        {
+            if ((framesCounter / 15) % 2) DrawRectangle(logoPositionX, logoPositionY, 16, 16, BLACK);
+        }
+        else if (state == 1)
+        {
+            DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, BLACK);
+            DrawRectangle(logoPositionX, logoPositionY, 16, leftSideRecHeight, BLACK);
+        }
+        else if (state == 2)
+        {
+            DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, BLACK);
+            DrawRectangle(logoPositionX, logoPositionY, 16, leftSideRecHeight, BLACK);
+
+            DrawRectangle(logoPositionX + 240, logoPositionY, 16, rightSideRecHeight, BLACK);
+            DrawRectangle(logoPositionX, logoPositionY + 240, bottomSideRecWidth, 16, BLACK);
+        }
+        else if (state == 3)
+        {
+            DrawRectangle(logoPositionX, logoPositionY, topSideRecWidth, 16, Fade(BLACK, alpha));
+            DrawRectangle(logoPositionX, logoPositionY + 16, 16, leftSideRecHeight - 32, Fade(BLACK, alpha));
+
+            DrawRectangle(logoPositionX + 240, logoPositionY + 16, 16, rightSideRecHeight - 32, Fade(BLACK, alpha));
+            DrawRectangle(logoPositionX, logoPositionY + 240, bottomSideRecWidth, 16, Fade(BLACK, alpha));
+
+            DrawRectangle(GetScreenWidth() / 2 - 112, GetScreenHeight() / 2 - 112, 224, 224, Fade(RAYWHITE, alpha));
+
+            DrawText(TextSubtext("raylib", 0, lettersCount), GetScreenWidth() / 2 - 44, GetScreenHeight() / 2 + 48, 50, Fade(BLACK, alpha));
+        }
+        else if (state == 4)
+        {
+            DrawRectangle(0, 0, screenWidth, screenHeight, Fade(BLACK, fadeOutAlpha));
+        }
+
+        EndDrawing();
+
+        if (state == 4 && fadeOutAlpha >= 1.0f) // Exit the loop when the screen is completely black
+        {
+            break;
+        }
+    }
+
+    CloseWindow(); // Close window and OpenGL context
 }
